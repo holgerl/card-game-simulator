50ceff8ca188a52e8d019ff2ccc34534d00127c5
Holger Ludvigsen <holger.ludvigsen@sparebank1.no>
2025-03-18 14:39:25 +0100

    Making CardGameSimulator more advanced with some statistics

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import { CardGameSimulator, RuleSet, Tactic } from "./simulator";
 import { CrazyEightsRuleSet, CrazyEightsDefaultTactic } from "./crazyeights";
+import { MonsterRuleSet, MonsterDefaultTactic } from "./monster";
 
-const ruleSet: RuleSet = new CrazyEightsRuleSet();
-const tactic: Tactic = new CrazyEightsDefaultTactic();
-const cardGameSimulator: CardGameSimulator = new CardGameSimulator();
-cardGameSimulator.simulate(2, ruleSet, tactic);
+
+const simulationSetup = {
+    nofPlayers: 2,
+    ruleSet: new MonsterRuleSet(),
+    tactic: new MonsterDefaultTactic(),
+}
+
+const cardGameSimulator: CardGameSimulator = new CardGameSimulator(0);
+cardGameSimulator.simulateOne(simulationSetup);
+//cardGameSimulator.simulateMany(100, simulationSetup);
--- simulator.ts
+++ simulator.ts
@@ -20,29 +20,99 @@ export interface Tactic {
     chooseMove(state: State, moves: Move[]): Move;
 }
 
-export class CardGameSimulator {
+export interface SimulationSetup {
+    nofPlayers: number;
+    ruleSet: RuleSet;
+    tactic: Tactic;
+}
 
-    public simulate(nofPlayers: number, ruleSet: RuleSet, tactic: Tactic): void {
+export interface SingleGameStats {
+    nofTurns: number;
+    winners: number[];
+}
+
+export interface MultipleGameStats {
+    nofTurnsMean: number;
+    nofTurnsStdDev: number;
+    victoryDistribution: number[];
+}
+
+function meanAndStandardDeviation(array: number[]): {mean: number, standardDeviation: number} {
+    const n = array.length
+    const mean = array.reduce((a, b) => a + b) / n
+    const standardDeviation = Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
+
+    return {mean, standardDeviation};
+}
+
+export class CardGameSimulator {
+    constructor(public logLevel = 0) {}
+
+    log(severity : number = 0, ...args: any[]) {
+        if (severity >= this.logLevel) {
+            console.log(...args);
+        }
+    }
+
+    public simulateOne({nofPlayers, ruleSet, tactic}: SimulationSetup): SingleGameStats {
         let state = ruleSet.makeStartState(nofPlayers);
         let nofTurns = 0;
 
-        console.log(state.toString());
+        this.log(0, state.toString());
         
         while (!ruleSet.isGameOver(state)) {
             const moves = ruleSet.listMoves(state);
-            console.log(`Moves: ${moves.join(", ")}`);
+            this.log(0, `Moves: ${moves.join(", ")}`);
             
             const move = tactic.chooseMove(state, moves);
-            console.log(`Chosen move: ${move.toString()}`);
+            this.log(0, `Chosen move: ${move.toString()}`);
             
             state = ruleSet.doMove(state, move);
-            console.log(state.toString());
+            this.log(0, state.toString());
 
             nofTurns++;
         }
+        
+        const stats: SingleGameStats = {
+            nofTurns,
+            winners: ruleSet.listWinners(state),
+        }
 
-        const winners = ruleSet.listWinners(state).map(playerIndex => `Player ${playerIndex}`);
+        const winnerNames = stats.winners.map(playerIndex => `Player ${playerIndex}`);
 
-        console.log(`Winners after ${nofTurns} turns: ${winners}`);
+        this.log(1, `Winners after ${nofTurns} turns: ${winnerNames}`);
+        
+        return stats;
+    }
+
+    public simulateMany(nofGames : number, simulationSetup: SimulationSetup): MultipleGameStats {
+        // @ts-ignore
+        const nofTurnsArray = new Array(nofGames).fill(0);
+        // @ts-ignore
+        const victoriesCount = new Array(simulationSetup.nofPlayers).fill(0);
+
+        for (let i = 0; i < nofGames; i++) {
+            const result = this.simulateOne(simulationSetup);
+            nofTurnsArray[i] = result.nofTurns;
+            result.winners.forEach(winner => {
+                victoriesCount[winner]++
+            });
+        }
+
+        let meanStdDev = meanAndStandardDeviation(nofTurnsArray);
+
+        let totalVictories = victoriesCount.reduce((a, b) => a + b);
+
+        const victoryDistribution = victoriesCount.map(count => count / totalVictories);
+
+        const stats: MultipleGameStats = {
+            nofTurnsMean: Math.round(meanStdDev.mean),
+            nofTurnsStdDev: parseFloat(meanStdDev.standardDeviation.toFixed(1)),
+            victoryDistribution,
+        }
+
+        this.log(2, `Stats after ${nofGames} games: ${JSON.stringify(stats)}`);
+
+        return stats;
     }
 }
